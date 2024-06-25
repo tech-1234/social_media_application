4f038495403859826b2ab8454e9a316415041e41
tech-1234 <ankitdutta1741@gmail.com>
2024-06-25 16:04:26 +0530

    Add Get All Posts endpoint

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -3,10 +3,89 @@ import { ApiError } from "../utils/ApiError.js";
 import { deleteOnCloudinary, uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Post } from '../models/post.model.js';
+import { User } from '../models/user.model.js';
+import mongoose, { isValidObjectId } from 'mongoose';
 
 
 const getAllPosts = asyncHandler(async (req, res) => {
+    const { page = 1, limit = 10, query = "", sortBy = "createdAt", sortType = 1, userId } = req.query;
 
+    // Ensure sortType is a number
+    const sortDirection = parseInt(sortType, 10);
+
+    // Match condition for the aggregation pipeline
+    const matchCondition = {
+        $or: [
+            { caption: { $regex: query, $options: "i" } },
+        ]
+    };
+
+    if (userId) {
+        matchCondition.owner = new mongoose.Types.ObjectId(userId);
+    }
+    let postAggregate;
+    try {
+        postAggregate = Post.aggregate([
+            {
+                $match: matchCondition
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "owner",
+                    foreignField: "_id",
+                    as: "owner",
+                    pipeline: [
+                        {
+                            $project: {
+                                _id: 1,
+                                fullName: 1,
+                                avatar: "$avatar.url",
+                                username: 1,
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $addFields: {
+                    owner: {
+                        $first: "$owner",
+                    },
+                },
+            },
+            // sorting according to sortType which is ascending in this case
+            {
+                $sort: {
+                    [sortBy]: sortDirection
+                }
+            }
+        ])
+    } catch (error) {
+        throw new ApiError(500, error.message || "Internal server error in post aggregation");
+    }
+    const options = {
+        page: parseInt(page, 10),
+        limit: parseInt(limit, 10),
+        customLabels: {
+            totalDocs: "totalPosts",
+            docs: "posts",
+        },
+        skip: (parseInt(page, 10) - 1) * parseInt(limit, 10),
+    };
+
+    Post.aggregatePaginate(postAggregate, options)
+        .then(result => {
+            if (result?.posts?.length === 0 && userId) {
+                return res.status(200).json(new ApiResponse(200, [], "No posts found"));
+            }
+
+            return res.status(200).json(new ApiResponse(200, result, "Post fetched successfully"));
+        })
+        .catch(error => {
+            console.log("Error:", error);
+            throw new ApiError(500, error?.message || "Internal server error in post aggregate paginate");
+        });
 })
 
 const publishAPost = asyncHandler(async (req, res) => {
